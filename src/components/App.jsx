9a5a8248cc6916ac6e3e7582a7bc5244043f98ea
Maksym Kolii <maksymkolii@gmail.com>
2022-12-08 00:02:08 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { GlobalStyle } from './Utils/GlobalStyle';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
@@ -9,16 +9,19 @@ import { Section } from './Section/Section';
 
 // model.id = nanoid(); //=> "V1StGXR8_Z5jdHi6B-myT"
 
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export function  App () {
+  // state = {
+  //   contacts: [
+  //     // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   filter: '',
+  // };
+
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState([]);
 
   componentDidMount() {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
@@ -41,12 +44,31 @@ export class App extends Component {
     }
   }
 
-  addContact = data => {
+  // addContact = data => {
+  //   const newContact = {
+  //     id: nanoid(),
+  //     ...data,
+  //   };
+  //   const isNameExist = this.state.contacts.find(({ name, number }) => {
+  //     // name.toLowerCase() === newContact.name.toLowerCase();
+  //     return (
+  //       name.toLowerCase() === newContact.name.toLowerCase() ||
+  //       number === newContact.number
+  //     );
+  //   });
+
+  //   isNameExist
+  //     ? window.alert(`${newContact.name} is alredy in contacts!`)
+  //     : this.setState(prevState => ({
+  //         contacts: [newContact, ...prevState.contacts],
+  //       }));
+  // };
+ const addContact = data => {
     const newContact = {
       id: nanoid(),
       ...data,
     };
-    const isNameExist = this.state.contacts.find(({ name, number }) => {
+    const isNameExist =contacts.find(({ name, number }) => {
       // name.toLowerCase() === newContact.name.toLowerCase();
       return (
         name.toLowerCase() === newContact.name.toLowerCase() ||
@@ -60,16 +82,37 @@ export class App extends Component {
           contacts: [newContact, ...prevState.contacts],
         }));
   };
-  deleteContact = iD => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== iD),
-    }));
+
+  // deleteContact = iD => {
+  //   this.setState(prevState => ({
+  //     contacts: prevState.contacts.filter(contact => contact.id !== iD),
+  //   }));
+  // };
+  const deleteContact = iD => {
+    setContacts(prevContacts => prevContacts.filter(({id}) => id !== iD))
+
   };
 
-  onFilterChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value,
-    });
+
+  // onFilterChange = ({ target: { name, value } }) => {
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
+
+  const onFilterChange = ev => {
+    const { name, value } = ev.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
   getFilteredContacts = () => {
@@ -85,9 +128,8 @@ export class App extends Component {
     return filter ? this.getFilteredContacts() : contacts;
   };
 
-  render() {
-    console.log('Render component');
-    const { filter } = this.state;
+    // console.log('Render component');
+    // const { filter } = this.state;
 
     // const options = filter ? this.getFilteredContacts() : contacts;
     const options = this.showContactsOptions();
@@ -95,19 +137,19 @@ export class App extends Component {
       <>
         <Section>
           <h1>Phonebook</h1>
-          <ContactForm addUser={this.addContact}></ContactForm>
+          <ContactForm addUser={addContact}></ContactForm>
         </Section>
         <Section>
           <h2>Contacts</h2>
-          <Filter filtered={filter} filterChange={this.onFilterChange}></Filter>
+          <Filter filtered={filter} filterChange={onFilterChange}></Filter>
           <ContactList
             options={options}
-            onClickDelete={this.deleteContact}
+            onClickDelete={deleteContact}
           ></ContactList>
         </Section>
 
         <GlobalStyle />
       </>
     );
-  }
+  
 }
